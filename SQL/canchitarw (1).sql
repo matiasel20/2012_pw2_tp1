3821821c6636e3555d159b226187d03122fff45e
matiasel20 <matiasel20@hotmail.com>
2012-05-14 18:28:57 -0300

    armado sql actualizado

---
+++ SQL/canchitarw (1).sql
@@ -0,0 +1,165 @@
+-- phpMyAdmin SQL Dump
+-- version 3.4.5
+-- http://www.phpmyadmin.net
+--
+-- Servidor: localhost
+-- Tiempo de generación: 14-05-2012 a las 23:27:45
+-- Versión del servidor: 5.5.16
+-- Versión de PHP: 5.3.8
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Base de datos: `canchitarw`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `alquiler`
+--
+
+CREATE TABLE IF NOT EXISTS `alquiler` (
+  `idAlquiler` int(11) NOT NULL AUTO_INCREMENT,
+  `cancha` int(1) unsigned DEFAULT NULL,
+  `fecha` timestamp NULL DEFAULT NULL,
+  `indumentaria` tinyint(1) DEFAULT NULL,
+  `duchas` tinyint(1) DEFAULT NULL,
+  `confiteria` tinyint(1) DEFAULT NULL,
+  `Cliente_idcliente` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`idAlquiler`,`Cliente_idcliente`),
+  KEY `fk_Alquiler_Cliente1` (`Cliente_idcliente`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- RELACIONES PARA LA TABLA `alquiler`:
+--   `Cliente_idcliente`
+--       `cliente` -> `idcliente`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `cliente`
+--
+
+CREATE TABLE IF NOT EXISTS `cliente` (
+  `idcliente` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `user` varchar(45) NOT NULL,
+  `nombre` varchar(45) NOT NULL,
+  `apellido` varchar(45) NOT NULL,
+  `dni` varchar(11) NOT NULL,
+  `fechanac` date NOT NULL,
+  `direccion` varchar(45) NOT NULL,
+  `localidad` varchar(45) NOT NULL,
+  `telcel` varchar(45) NOT NULL,
+  `email` varchar(45) NOT NULL,
+  `password` varchar(45) NOT NULL,
+  PRIMARY KEY (`idcliente`),
+  UNIQUE KEY `user_UNIQUE` (`user`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='\n	\n					' AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `compra`
+--
+
+CREATE TABLE IF NOT EXISTS `compra` (
+  `idclienteCompra` int(10) unsigned NOT NULL,
+  `cantidad` int(10) unsigned NOT NULL,
+  `fecha` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `Producto_idProducto1` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`idclienteCompra`),
+  KEY `fk_Producto_copy1_has_Cliente_copy1_Cliente_copy11` (`idclienteCompra`),
+  KEY `fk_Compra_Producto1` (`Producto_idProducto1`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- RELACIONES PARA LA TABLA `compra`:
+--   `Producto_idProducto1`
+--       `producto` -> `idProducto`
+--   `idclienteCompra`
+--       `cliente` -> `idcliente`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `empleado`
+--
+
+CREATE TABLE IF NOT EXISTS `empleado` (
+  `idEmpleado` int(11) NOT NULL,
+  `nombre` varchar(45) NOT NULL,
+  `apellido` varchar(45) NOT NULL,
+  `dni` varchar(45) NOT NULL,
+  `direccion` varchar(45) NOT NULL,
+  `fechaNac` varchar(45) NOT NULL,
+  `tel/cel` varchar(45) NOT NULL,
+  `usuario` varchar(45) NOT NULL,
+  `password` varchar(45) NOT NULL,
+  `email` varchar(45) NOT NULL,
+  PRIMARY KEY (`idEmpleado`),
+  UNIQUE KEY `usuario_UNIQUE` (`usuario`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `producto`
+--
+
+CREATE TABLE IF NOT EXISTS `producto` (
+  `idProducto` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `codigo` int(10) unsigned NOT NULL,
+  `descripcion` varchar(45) NOT NULL,
+  `modelo` varchar(45) NOT NULL,
+  `tamanio` varchar(45) DEFAULT NULL,
+  `precio` decimal(5,2) unsigned NOT NULL,
+  `stock` int(45) NOT NULL,
+  PRIMARY KEY (`idProducto`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `torneo`
+--
+
+CREATE TABLE IF NOT EXISTS `torneo` (
+  `idTorneo` int(11) NOT NULL AUTO_INCREMENT,
+  `temporada` timestamp NULL DEFAULT NULL,
+  `torneo` varchar(45) DEFAULT NULL,
+  `campeon` varchar(45) NOT NULL,
+  `subcampeon` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`idTorneo`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `alquiler`
+--
+ALTER TABLE `alquiler`
+  ADD CONSTRAINT `alquiler_ibfk_1` FOREIGN KEY (`Cliente_idcliente`) REFERENCES `cliente` (`idcliente`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `compra`
+--
+ALTER TABLE `compra`
+  ADD CONSTRAINT `compra_ibfk_2` FOREIGN KEY (`Producto_idProducto1`) REFERENCES `producto` (`idProducto`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `compra_ibfk_1` FOREIGN KEY (`idclienteCompra`) REFERENCES `cliente` (`idcliente`) ON DELETE CASCADE ON UPDATE NO ACTION;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
