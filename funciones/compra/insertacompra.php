1551146a2e6c06ff85fc9a76f408c9bc330b65c3
Gabriel <jim_morrison34417@hotmail.com>
2012-05-17 16:09:10 -0300

    Update funciones/compra/insertacompra.php

--- funciones/compra/insertacompra.php
+++ funciones/compra/insertacompra.php
@@ -1,20 +1,11 @@
-<?php
-?>
-
-<form action="funciones/compra/alta.php" method="post">
+<form action="alta.php" method="post">
+            
             cantidad<br>
             <input type="text" name="cantidad" ><br>
-
-                        fecha<br>
-            <input type="text" name="fecha" ><br>
-            
-                        cliente<br>
+            cliente<br>
             <input type="text" name="cliente" ><br>
-            
-                        producto<br>
+            producto<br>
             <input type="text" name="producto" ><br>
-            
-           
             
             <input type="submit" value="ingresar">
         </form>
