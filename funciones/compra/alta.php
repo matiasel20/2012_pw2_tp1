076bee8fa68541b1f78c7e043178f8772de4d580
gabriel <jimbodie06@hotmail.com>
2012-05-17 00:55:57 -0300

    arreglando el modelo de bd,,, agregando alta de compra

---
+++ funciones/compra/alta.php
@@ -0,0 +1,8 @@
+<?php
+include("../conectar.php");
+extract($_POST);
+//session_start();
+$pdo=conectar();
+?>
+
+
---
+++ funciones/compra/insertacompra.php
@@ -0,0 +1,20 @@
+<?php
+?>
+
+<form action="funciones/compra/alta.php" method="post">
+            cantidad<br>
+            <input type="text" name="cantidad" ><br>
+
+                        fecha<br>
+            <input type="text" name="fecha" ><br>
+            
+                        cliente<br>
+            <input type="text" name="cliente" ><br>
+            
+                        producto<br>
+            <input type="text" name="producto" ><br>
+            
+           
+            
+            <input type="submit" value="ingresar">
+        </form>
