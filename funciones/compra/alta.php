a9bd6257e74ab7537346fc773a2544ee011db21d
gabriel <jimbodie06@hotmail.com>
2012-05-17 01:05:56 -0300

    terminando la insercion de compra,,, recordad q hay q definir el modelo de bd para q acepte datos nulos!,,, uguu~ tengo sueño

--- funciones/compra/alta.php
+++ funciones/compra/alta.php
@@ -3,6 +3,32 @@ include("../conectar.php");
 extract($_POST);
 //session_start();
 $pdo=conectar();
+
+ try {
+    $pdo->beginTransaction();
+    $sql="INSERT INTO compra (cantidad,fecha,clienteid,productoid)
+      VALUES (:cantidad,:fecha,:clienteid,:productoid)"; 
+     $stmt = $pdo->prepare($sql);
+      //$stmt->setFetchMode(PDO::FETCH_ASSOC);
+       $stmt->bindParam(':cantidad', $cantidad);
+       $stmt->bindParam(':fecha',$fecha);
+       $stmt->bindParam(':clienteid',$clienteid);
+       $stmt->bindParam(':productoid',$productoid);
+
+       
+       $stmt->execute();
+       
+   
+     
+  $pdo->commit();  //se guardaría todo “definitivamente”
+
+     header('Location: ../../administracion.php');
+} catch (PDOException $e) {
+  $pdo->rollBack();  //ante cualquier excepción, revierte todo
+   echo 'La operación ha fallado: ' . $e->getMessage();
+
+}
+
 ?>
 
 
