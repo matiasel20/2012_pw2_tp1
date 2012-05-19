b74d1a40f9c8ba4b703b48d495f94b5de3d74ad8
seba <seba_rw84@hotmail.com>
2012-05-19 16:35:09 -0300

    archivo que me falto en el commit anterior

---
+++ funciones/login/consultaloginempleado.php
@@ -0,0 +1,38 @@
+<?php
+
+//session_start();
+
+function consultaloginempleado($pdo) {
+    
+     try {
+        
+        $sql = "SELECT apellido FROM empleado WHERE usuario = :usuario AND password = :pass";
+
+        //preparamos un statement con el sql anterior
+        $stmt = $pdo->prepare($sql);
+
+        //especificamos la salida como un array
+        $stmt->setFetchMode(PDO::FETCH_ASSOC);//podria ser..
+
+        //sutituimos lso parametros con los valores reales
+        $stmt->bindParam(':usuario',$_POST['usuario']);
+        $stmt->bindParam(':pass',$_POST['pass']);
+
+        //ejecutamos la consulta
+        $stmt->execute();
+
+        //recuperamos los datos en el array asoc.
+        $result = $stmt->fetchAll();        
+        
+    } catch(PDOException $e) {
+               echo ("Error al realizar la consulta: ".$e->getMessage());   
+               return 0;
+    } 
+    
+    if (!$result){
+        return 0;
+    }
+    return 1;
+}
+
+?>
