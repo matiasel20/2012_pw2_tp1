0a661768c56ab3940c6a87a3d24088d2cfe0e0c4
seba <seba_rw84@hotmail.com>
2012-05-24 18:46:03 -0300

    crea base si no existe

--- SQL/canchitarw.sql
+++ SQL/canchitarw.sql
@@ -15,6 +15,9 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
 
+
+CREATE DATABASE IF NOT EXISTS canchitarw;
+USE canchitarw;
 --
 -- Base de datos: `canchitarw`
 --
